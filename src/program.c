f3a6b237277b2c026b7421e63e574169583cb6f2
meriiav <maria.aran01@estudiant.upf.edu>
2026-01-30 22:51:54 +0100

    cb works (still needs changes)

--- src/program.c
+++ src/program.c
@@ -45,36 +45,66 @@
 
     long long process_text(int fd, int bufferSize){
         long long sum = 0;
-        CircularBuffer cb;
-        if(buffer_init(&cb,bufferSize)!=0){
+        CircularBuffer circbuf;
+        if(buffer_init(&circbuf,bufferSize)!=0){
             perror("buffer_init");
             return 0;
             }
-        unsigned char circbuf;
+
+        unsigned char character;
         ssize_t bytes;
+
+
         while(1){
-            bytes = read(fd, &circbuf, 1);
+            bytes = read(fd, &character, 1);
             if (bytes==0){
-                if (buffer_used_bytes(&cb)>0){
-                    sum+=buffer_read_number(&cb, ',', 1);
+                int size=buffer_size_next_element(&circbuf,',',1);
+                if(size!=-1){
+                    char str[64]; //
+                    int i=0;
+                    while(buffer_used_bytes(&circbuf)>0){
+                        str[i++]=buffer_pop(&circbuf);
+                    }
+                    str[i]='\0';
+                    sum+=atoll(str);
+                    }
+                    break;
                 }
-                break;
-            }
+
+
             if (bytes<0){
                 perror("read");
                 break;
             }
-            buffer_push(&cb, circbuf);
-            if (circbuf ==','){
-                sum+=buffer_read_number(&cb,',', 0);
+
+            if (buffer_free_bytes(&circbuf)>0){
+                buffer_push(&circbuf,character);
             }
+
+            if(character==','){
+                char str[64];
+                int i=0;
+
+                while(buffer_used_bytes(&circbuf)>0){
+                    char character2 = buffer_pop(&circbuf);
+                    if(character2!=','){
+                        str[i++]=character2;
+
+                    }
+                    else{
+                        break;
+                    }
+                }
+                str[i]='\0';
+                sum+=atoll(str);
         }
-        buffer_deallocate(&cb);
+     }
+        buffer_deallocate(&circbuf);
         return sum;
 }
 
     int main(int argc, char* argv[]){
-        const char* file = "test_small.dat";
+        const char* file = "int_text_small.txt";
         int fd;
         fd = open(file, O_RDONLY);
 
@@ -83,7 +113,7 @@
             return 1;
         }
         printf("File abierto: fd= %d\n",fd);
-        printf("%lld\n",process_binary(fd,1024));
+        printf("%lld\n",process_text(fd,1024));
 
         close(fd);
         return 0;
