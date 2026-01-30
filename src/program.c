473a49aeb2a3dee50ad27061e4ce9ff90b847771
meriiav <maria.aran01@estudiant.upf.edu>
2026-01-30 19:12:11 +0100

    process binary fixed

--- src/program.c
+++ src/program.c
@@ -1,43 +1,55 @@
+    #include <unistd.h>
     #include <stdio.h>
     #include <stdlib.h>
     #include <fcntl.h>
     #include <unistd.h>
     #include "circularBuffer.h"
 
-
     long long process_binary(int fd, int bufferSize){
-        long long sum = 0;
+        unsigned long long sum = 0;
         int intSize=sizeof(int);
-
         //we adjust the buffer size to only fit whole integers (4 bytes)
         int adjustedBuffer=bufferSize-(bufferSize % intSize);
-        unsigned char *buf = malloc(adjustedBuffer);  //buffer is pointer to raw bytes
+        if(adjustedBuffer<=0){
+            adjustedBuffer=intSize;
+
+        }
+        unsigned char *buf = malloc(adjustedBuffer); //buffer is pointer to raw bytes
         if(!buf){
             perror("malloc");
             return 0;
         }
-        
+
         ssize_t bytesRead;
         while((bytesRead=read(fd,buf,adjustedBuffer))>0){
             int count=bytesRead/sizeof(int);
             int *n=(int*)buf;
-            for(int i = 0; i < count; i++){
-                sum+=n[i];
-            } 
+
+            unsigned int unsignedsum = (unsigned int)sum;
+            for(int i=0; i<count; i++){
+                unsignedsum+=(unsigned int)n[i];
+            }
+
+            sum =(long long)(int)unsignedsum;
         }
+
+        if(bytesRead<0){
+            perror("read");
+        }
+
+        free(buf);
         return sum;
     }
 
+
+
     long long process_text(int fd, int bufferSize){
-
-       long long sum = 0;
-       CircularBuffer cb;
-
+        long long sum = 0;
+        CircularBuffer cb;
         if(buffer_init(&cb,bufferSize)!=0){
             perror("buffer_init");
             return 0;
-        }
-        
+            }
         unsigned char circbuf;
         ssize_t bytes;
         while(1){
@@ -59,7 +71,7 @@
         }
         buffer_deallocate(&cb);
         return sum;
-    }
+}
 
     int main(int argc, char* argv[]){
         const char* file = "test_small.dat";
@@ -77,3 +89,4 @@
         return 0;
         
     }
+    
