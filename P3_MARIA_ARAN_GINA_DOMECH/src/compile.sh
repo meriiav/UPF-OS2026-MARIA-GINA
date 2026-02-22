778390c1a6c801f824e7aa0de0cb8c4b58ee1ca4
meriiav <maria.aran01@estudiant.upf.edu>
2026-02-22 23:03:47 +0100

    integrants

--- P3_MARIA_ARAN_GINA_DOMECH/src/compile.sh
+++ P3_MARIA_ARAN_GINA_DOMECH/src/compile.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# Compile the parallel histogram program with pthread support
-
-# Compile the program in the current folder
 gcc P3_Parallel.c parsePGM.c -o computeHistogramParallel -lpthread
