c1ea3ac2da48608037c5aad8996d36b53a6da15b
Ginadomech <gina.domech01@estudiant.upf.edu>
2026-02-24 18:18:51 +0100

    hjhg

--- P4_MARIA_ARAN_GINA_DOMECH/producer.c
+++ P4_MARIA_ARAN_GINA_DOMECH/producer.c
@@ -1,3 +1,5 @@
+#include producer.h
+
 int blockSize = 1024 * 16;
 // Position from which the Producer will try to read a block. This needs to be initialised to just after the header.
 int readPos; 
@@ -24,12 +26,30 @@ void * Producer (void* arg) {
         }
         
         //Consumer part of adding to the buffer: need to 
-        while(nBuffer == elementsInBuffer); // Infinite wait, change
-        /* 
-        To complete by the student: should insert the element to the buffer
-        */
+        while (elementsInBuffer == nBuffer){ 
+            pthread_cond_wait(&not_full, &lock_buffer);
+        }
+        buffer[tail] = buff;
+        bufferSizes[tail] = nBytesRead;
+
+        tail = (tail + 1) % nBuffer;
         elementsInBuffer++;
+
+        pthread_cond_signal(&not_empty);
+        pthread_mutex_unlock(&lock_buffer);
     }
     // If exiting, make sure you wake up all sleeping threads before exiting 
     // (and that they don't go to sleep if the finishes)
+
+    close(fd);
+
+    pthread_mutex_lock(&lock_buffer);
+    activeProducers--;
+
+    if(activeProducers == 0)
+        pthread_cond_broadcast(&not_empty);
+
+    pthread_mutex_unlock(&lock_buffer);
+
+    pthread_exit(NULL);
 }
